f4aa93bcaa95ff21de83d23df015836e92d13e80
wulong yang <yangwulong0418@gmail.com>
2017-04-03 10:03:58 +1000

    Initial Commit

---
+++ Q6JobService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Q6JobService
+//
+//  Created by yang wulong on 3/4/17.
+//  Copyright © 2017 q6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
