8f7b1ff57c4fec3a1acf839d7ba8692625407c7a
wulong yang <yangwulong0418@gmail.com>
2017-05-11 10:44:40 +1000

    222

---
+++ Q6JobService/JobSearchBar.swift
@@ -0,0 +1,39 @@
+//
+//  JobSearchBar.swift
+//  Q6JobService
+//
+//  Created by yang wulong on 11/5/17.
+//  Copyright © 2017 q6. All rights reserved.
+//
+
+import UIKit
+
+class JobSearchBar: UISearchBar {
+
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        setUpView()
+        
+        
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    func setUpView(){
+        self.layer.cornerRadius = 5;
+        self.layer.borderWidth = 0.1;
+        self.layer.borderColor = UIColor.black.cgColor
+        
+    }
+
+}
